0f7e8ed330dd94826ce2033a388a696dadc467dc
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2019-04-22 13:16:03 -0400

    Blacken

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -4,25 +4,27 @@ import pytest
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+    os.environ["MOLECULE_INVENTORY_FILE"]
+).get_hosts("all")
 
 
-@pytest.mark.parametrize('pkg', [
-    'logrotate'
-])
+@pytest.mark.parametrize("pkg", ["logrotate"])
 def test_packages(host, pkg):
     package = host.package(pkg)
 
     assert package.is_installed
 
 
-@pytest.mark.parametrize('file,content', [
-    ('/etc/logrotate.conf', '^# rotate log files daily'),
-    ('/etc/logrotate.conf', '^daily'),
-    ('/etc/logrotate.conf', '^# keep 30 days worth of backlogs'),
-    ('/etc/logrotate.conf', '^rotate 30'),
-    ('/etc/logrotate.conf', '^compress')
-])
+@pytest.mark.parametrize(
+    "file,content",
+    [
+        ("/etc/logrotate.conf", r"^# rotate log files daily"),
+        ("/etc/logrotate.conf", r"^daily"),
+        ("/etc/logrotate.conf", r"^# keep 30 days worth of backlogs"),
+        ("/etc/logrotate.conf", r"^rotate 30"),
+        ("/etc/logrotate.conf", r"^compress"),
+    ],
+)
 def test_files(host, file, content):
     f = host.file(file)
 
@@ -30,10 +32,13 @@ def test_files(host, file, content):
     assert f.contains(content)
 
 
-@pytest.mark.parametrize('file,content', [
-    ('/etc/logrotate.d/rsyslog', '^\s*daily'),
-    ('/etc/logrotate.d/rsyslog', '^\s*rotate 7'),
-])
+@pytest.mark.parametrize(
+    "file,content",
+    [
+        ("/etc/logrotate.d/rsyslog", r"^\s*daily"),
+        ("/etc/logrotate.d/rsyslog", r"^\s*rotate 7"),
+    ],
+)
 def test_rsyslog_file(host, file, content):
     f = host.file(file)
 
